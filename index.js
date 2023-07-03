b55fb8d4516e4109f912f88877d28a7cabf69847
SIVARAMAN ALIAS RAJA S <127581183+Sivaramanraja98@users.noreply.github.com>
2023-07-03 13:30:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 
 //MongoDB connection
-mongoose.connect("mongodb://0.0.0.0:27017/studentmentor", { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/studentmentor", { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
   console.log('Connected to MongoDB');
 })
